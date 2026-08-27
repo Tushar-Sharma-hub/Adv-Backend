0bd33244d371aace00d72969ac8e05d2ba959d9b
Tushar Sharma <ts817479@gmail.com>
2026-08-28 03:18:22 +0530

    sd

--- lvl3_SystemDesign/phase2_MicroServices/backend/gateway/index.js
+++ lvl3_SystemDesign/phase2_MicroServices/backend/gateway/index.js
@@ -8,12 +8,12 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.get("/", (req, res) => {
-  res.send("Hello from Backend/Gateway!");
+  res.send(`Hello from ${process.env.SERVER_NAME}!`);
 });
 
-app.use("/auth",proxy("http://localhost:8001"))
-app.use("/order",proxy("http://localhost:8002"))
-app.use("/product",proxy("http://localhost:8003"))
+app.use("/auth",proxy("http://auth-service:8001"))
+app.use("/order",proxy("http://order-service:8002"))
+app.use("/product",proxy("http://product-service:8003"))
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
