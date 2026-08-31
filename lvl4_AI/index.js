abac9b30ab7fc4563f4a1d5f596271b2bc78b482
Tushar Sharma <ts817479@gmail.com>
2026-08-31 11:12:19 +0530

    lilu

--- lvl4_AI/index.js
+++ lvl4_AI/index.js
@@ -1,10 +1,29 @@
+import "dotenv/config";
 import express from "express";
+import { GoogleGenAI } from "@google/genai";
 
 const app = express();
-const PORT = 5000;
+const PORT = 5050;
+
+const ai = new GoogleGenAI({
+  apiKey: process.env.GEMINI_API_KEY
+});
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello from lvl4!");
+});
+
+app.post("/ai", async (req, res) => {
+  const { prompt } = req.body;
+
+  const interaction = await ai.interactions.create({
+    model: "gemini-3.7-flash",
+    input: prompt,
+  });
+
+  res.jsom({'ai':interaction.output_text});
 });
 
 app.listen(PORT, () => {
