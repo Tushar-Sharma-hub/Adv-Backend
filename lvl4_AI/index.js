19911e15f4c2c9bfd0da84af87730da434ef78cb
Tushar Sharma <ts817479@gmail.com>
2026-09-04 17:47:52 +0530

    lc

--- lvl4_AI/index.js
+++ lvl4_AI/index.js
@@ -2,6 +2,7 @@ import "dotenv/config";
 import express from "express";
 import { GoogleGenAI } from "@google/genai"; //without langchain
 import { ChatGoogle } from "@langchain/google"; //with langchain
+import { ChatGroq } from "@langchain/groq";
 
 const app = express();
 const PORT = 5050;
@@ -37,18 +38,33 @@ app.get("/", (req, res) => {
 // });
 
 //with langchain
-const llm = new ChatGoogle({
-  apiKey: process.env.GOOGLE_API_KEY,
-  model: "gemini-3.7-flash",
+const llm = new ChatGroq({
+  model: "openai/gpt-oss-120b",
+  temperature: 0.7, //temperature controls the randomness of the output. Higher values (e.g., 0.8) make the output more random, while lower values (e.g., 0.2) make it more focused and deterministic.
+  maxTokens: 100, //maxTokens limits the length of the generated output.
+  maxRetries: 3, //maxRetries specifies the number of times to retry the request in case of failures.
 });
 app.post("/ai", async (req, res) => {
   const { prompt } = req.body;
 
-  const response = await llm.call(prompt);
+  const response = await llm.invoke([
+    //We can set roles, like system,user,human and assistant. 
+    //System role is used to set the behavior of the model, 
+    // user role is used to provide input from the user, 
+    // human role is used to provide input from a human, and
+    // assistant role is used to provide input from an AI assistant.
+    {
+      role: "system", 
+      content: "You are a helpful assistant.Your name is Oggy. If you don't know the answer then don't give incorrect answer."
+  },
+    {
+      role: "human", 
+      content: prompt
+    }
+  ]);
 
   res.json({ ai: response });
 });
-
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
