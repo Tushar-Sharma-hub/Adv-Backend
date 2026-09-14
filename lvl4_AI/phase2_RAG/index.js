ae85377878b0f8c1cefb291867e28f87d0521c2e
Tushar Sharma <ts817479@gmail.com>
2026-09-14 18:50:11 +0530

    aiComplete

--- lvl4_AI/phase2_RAG/index.js
+++ lvl4_AI/phase2_RAG/index.js
@@ -4,6 +4,10 @@ import { ChatGroq } from "@langchain/groq"
 import fs from "fs";
 import { PDFParse } from 'pdf-parse';
 import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
+import { GoogleGenerativeAIEmbeddings } from "@langchain/google-genai";
+import { TaskType } from "@google/generative-ai";
+import { QdrantVectorStore } from "@langchain/qdrant";
+import { HumanMessage, SystemMessage } from "langchain";
 
 dotenv.config()
 const app = express()
@@ -17,6 +21,17 @@ const llm = new ChatGroq({
     maxRetries: 2
 })
 
+const embeddings = new GoogleGenerativeAIEmbeddings({
+  model: "gemini-embedding-001",
+  taskType: TaskType.RETRIEVAL_DOCUMENT,
+  title: "Document title",
+});
+
+const vectorStore = await QdrantVectorStore.fromExistingCollection(embeddings, {
+  url: process.env.QDRANT_URL,
+  collectionName: "Grocery-Store",
+});
+
 const upload = async () =>{
     const pdfPath = "./knowledge.pdf"
     const buffer = fs.readFileSync(pdfPath)
@@ -28,16 +43,23 @@ const upload = async () =>{
         chunkOverlap: 50, //overlap is 50 characters , means last 50 characters from the previous chunk will be included in the next chunk
     });
     const docs = await splitter.createDocuments([text]);
-    console.log("docs", docs)
+    await vectorStore.addDocuments(docs); 
 }
-upload()
+// upload() upload the pdf to qdrant vector store - 1 time
 
 app.post("/ai", async (req, res) => {
     const { input } = req.body
 
-    const response = await llm.invoke(input)
+    const docs = await vectorStore.similaritySearch(input, 10); //search for top 10 similar documents
+    const context = docs.map((doc) => doc.pageContent).join("\n");
 
-    return res.status(200).json({ "ai:": response.content })
+    const response = await llm.invoke([
+        new SystemMessage(`You are a helpful assistant. Use the following context to answer the question.
+            Do not use any outside knowledge. If the answer is not in the context, say "I don't know". Context: ${context}`),
+        new HumanMessage(input)
+    ])
+
+    return res.status(200).json({ai: response.content})
 })
 
 app.get("/", (req, res) => {
